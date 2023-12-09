1d486978b2d5a9120b618447529d2579d57be6e3
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-12-09 14:07:14 -0500

    -

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const authMiddleware = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+}
+
+startApolloServer();
