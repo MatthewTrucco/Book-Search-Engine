488158c90774099848111cd6fbe57ef50f6655ac
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-12-09 23:59:04 -0500

    server.js update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
-const { typeDefs, resolvers } = require('./schemas');
-const authMiddleware = require('./utils/auth');
+const db = require('../config/connection'); // Adjusted path
+const { typeDefs, resolvers } = require('../schemas'); // Adjusted path
+const authMiddleware = require('../utils/auth'); // Adjusted path
 const helmet = require('helmet'); // Import helmet
 
 const app = express();
@@ -40,18 +40,20 @@ app.use(express.json());
 
 // Serve static files from the 'dist' directory in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'dist')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
-// Favicon handling (if you have a favicon.ico, place it in the 'dist' folder)
+// Favicon handling (if you have a favicon.ico, place it in the 'client/dist' folder)
 app.get('/favicon.ico', (req, res) => {
-  // Send a 204 No Content status if you don't have a favicon.ico
+  // You can send a 204 No Content status if you don't have a favicon.ico
   res.status(204).send();
+  // Or if you have a favicon.ico, you can uncomment the following line:
+  // res.sendFile(path.resolve(__dirname, '../client/dist/favicon.ico'));
 });
 
 // Catch-all route to serve the main index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
 // Start Apollo Server
